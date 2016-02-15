bdbc5c07d1c1bd9e9fa0716df52b3ba8b1732096
Michael <clarantiriniga@yahoo.com>
2016-02-15 12:58:27 +0300

    new changes

---
+++ app/src/main/java/com/example/michael/bucket/MIKE.java
@@ -0,0 +1,9 @@
+package com.example.michael.bucket;
+
+/**
+ * Created by Michael on 2/15/2016.
+ */
+public class MIKE {
+    String mike;
+
+}
--- app/src/main/java/com/example/michael/bucket/MainActivity.java
+++ app/src/main/java/com/example/michael/bucket/MainActivity.java
@@ -8,9 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+  Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
